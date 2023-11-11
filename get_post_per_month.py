3b542c17ee1a21dd65b73efffcdf8de80829bbed
Davron17 <dorazaliyev75@gmail.com>
2023-11-11 20:01:02 +0500

    per_month

--- get_post_per_month.py
+++ get_post_per_month.py
@@ -1,7 +1,7 @@
-from read_data import fromJson
+from read_data import fromJson as data
+import datetime
 
-
-def get_post_per_month(data:dict)->dict:
+def get_post_per_month(data:dict,month:int)->int:
     """
     Return the number of posts for each month
 
@@ -11,4 +11,16 @@ def get_post_per_month(data:dict)->dict:
     Returns: 
         dict: a dictionary with the number of posts for each month
     """
-    return
+    dic = {i: 0 for i in range(1, 13)}
+
+    for message in data.get("messages", []):
+        if message.get("type") == "message" and "date_unixtime" in message:
+            # Assuming "date_unixtime" field is present in the message
+            m = datetime.datetime.fromtimestamp(int(message["date_unixtime"])).month
+            dic[m] += 1
+
+    return dic
+
+data_dict = data("data/result.json")
+posts_per_month = get_post_per_month(data_dict,0)
+print(posts_per_month)
